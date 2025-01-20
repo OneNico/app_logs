559f21f510accea48f4d4487401e99d83c9b3183
OneNico <69123659+OneNico@users.noreply.github.com>
2025-01-20 11:26:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+
+import streamlit as st
+from src.ui.visual import render_ui
+
+def main():
+    """
+    Función principal que ejecuta la interfaz de usuario.
+    """
+    render_ui()
+
+if __name__ == "__main__":
+    main()
